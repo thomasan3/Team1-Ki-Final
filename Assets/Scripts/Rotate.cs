bdda74e0cd6ebd6a1ec540a3d78aa3fee4bdcaeb
Lorelai Davis <lorelai.anne03@gmail.com>
2025-04-25 01:16:02 -0400

    rotate/hand interaction/test textures/fix right leg rigging

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    public float rotationSpeed = 50.0f; // Adjust this value to control rotation speed
+    public float rotationSpeed = 10.0f; // Adjust this value to control rotation speed
     void Update()
     {
         // Rotate around the Y-axis
